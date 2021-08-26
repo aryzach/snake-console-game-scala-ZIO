13393c7cfd260ba734e7f34e13646b6c0079eec1
aryzach <zach_asmith@yahoo.com>
2021-08-25 22:41:20 -0400

    added random food after eating

--- src/main/scala/com/example/Game.scala
+++ src/main/scala/com/example/Game.scala
@@ -2,14 +2,13 @@ package com.example.game
 
 import zio._
 import zio.stream.ZStream
+import zio.stream._
 import zio.console._
 import zio.clock._
 import zio.duration._
 import zio.random._
 import java.io.IOException
 
-
-import scala.concurrent.duration.DurationLong
 import scala.collection.immutable.Queue
 
 import com.example.game.Event._
@@ -17,11 +16,11 @@ import com.example.utils.TypeAlias._
 
 sealed trait Event
 object Event {
-  case object Tick extends Event
   case class UserAction(key: Position) extends Event 
+  case class Food(p: Position) extends Event
 }
 
-sealed case class Position(x: Int, y: Int) {
+sealed  case class Position(x: Int, y: Int) {
   val stringify = x.toString + " " + y.toString
   def up: Position = Position(x - 1, y)
   def down: Position = Position(x + 1, y)
@@ -44,71 +43,64 @@ sealed case class Position(x: Int, y: Int) {
   def equals(p: Position) = (x == p.x) && (y == p.y)
 }
 
-/*
-sealed case class Parts(p: Queue[Position]) {
-  def advance(d: Position)(w: Int)(h: Int) = Parts(p.map(v => v.update(d)(w)(h)))
-  def contains(v: Position): Boolean = p.contains(v)
-  val head = p.head
-  def push(d: Position) = Parts(d +: p)
-}
-*/
-
-
 
 sealed case class GameState (parts: Parts, direction: Position, width: Int, height: Int, food: Position) {
-  def update() = {
+  def update(newFood: Position) = {
     val h = parts.last
     if (food.equals(h)) {
-      this.copy(parts = parts.enqueue(h.add(direction)).map(p => p.wrap(width)(height)), food = Position(10,10))
+      this.copy(parts = parts.enqueue(h.add(direction)).map(p => p.wrap(width)(height)), food = newFood)
     } else {
       this.copy(parts = parts.enqueue(h.add(direction)).dequeue._2.map(p => p.wrap(width)(height)))
     }
   }
 
+
 }
 
 object Game {
-  val downInterval = 2
-  val linesPerLevel = 10
+//  val downInterval = 2
+//  val linesPerLevel = 10
 }
 
 class Game(width: Int, height: Int, interactions: ZStream[Console, IOException, Position])  {
 
   val initialParts = Queue(Position(3,4))
-  val initialState = GameState(initialParts, Position(0,0).left, width, height, Position(1,1))
+  val initialState: GameState = GameState(initialParts, Position(0,0).left, width, height, Position(1,1))
 
   /**
    * This stream reflects all changes in the state of game field.
    */
-  val gameStates: ZStream[Console with Clock, IOException, GameState] = {
+  val gameStates: ZStream[Console with Clock with Random, IOException, GameState] = {
 
-    // Two sources of events:
-    // 1. Regular ticks
-    val tick: ZStream[Clock, Nothing, Event] = ZStream.tick(125.millis).map(_ => Tick)
-    // 2. User's interactions
-    val userMoves: ZStream[Console, IOException, UserAction] = interactions.map(x => UserAction(x))
-    // 3. Random food
-    /*
-    val randomPositions: ZStream[Has[Random], Nothing, Int] = 
-      ZStream.repeatZIO {
+    // Two sources of events and "precomputed" random Food:
+    // Random food
+    // maybe a stream isn't the best approach? Is this more or less efficient than getting a random Food only when needed? If the stream is precomputed in Chunks, maybe it's more efficient?
+    val foodStream: ZStream[Random, Nothing, Food] = 
+      ZStream.repeatEffect {
         for {
-          x <- nextIntBounded(5)
-        } yield (x)
+          x <- nextIntBounded(width)
+          y <- nextIntBounded(height)
+        } yield Food(Position(x,y))
       } 
-*/
+    // Regular ticks
+    val tick: ZStream[Clock, Nothing, Unit] = ZStream.tick(125.millis)
+    // Timed Food
+    val tickedFood: ZStream[Clock with Random, Nothing, Food] = tick.zip(foodStream).map(x => x._2)
+    // User's interactions
+    val userMoves: ZStream[Console, IOException, UserAction] = interactions.map(x => UserAction(x))
     // merge them
-    val allEvents: ZStream[Console with Clock, IOException, Event] = tick merge userMoves
+    val allEvents: ZStream[Console with Clock with Random, IOException, Event] = tickedFood merge userMoves
 
-    val states = allEvents.scan(initialState)(nextState)
+    val states: ZStream[Console with Clock with Random, IOException, GameState] = allEvents.scan(initialState)(nextState)
+
     states.takeWhile(_.direction != false)
   }
-
   /**
    * Compute next game state given the previous state and an event.
    */
   private def nextState(state: GameState, event: Event): GameState = {
     event match {
-      case Tick => state.update()
+      case Food(p) => state.update(p)
       case UserAction(direction) => state.copy(direction = direction)
     }
   }
--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -9,16 +9,53 @@ import java.io.IOException
 import zio.console._
 import zio.clock._
 import zio.stream._
+import zio.random._
 
 object Main extends App {
 
-  override def run(args: List[String]): ZIO[Console with Clock, Nothing, ExitCode] = {
+  override def run(args: List[String]): ZIO[Console with Clock with Random, Nothing, ExitCode] = {
     val game = new Game(20, 20, UserInput.moves)
     for {
       _ <- Screen.draw(Draw.eraseScreen)
-      a <- game.gameStates.tap(s => Screen.render(s)).either.fold(ZIO.succeed(()))((x,y) => ZIO.succeed(())).exitCode
+      a <- game.gameStates.tap(s => Screen.render(s)).either.fold(ZIO.succeed(()))((_,_) => ZIO.succeed(())).exitCode
     } yield a
   }
 }
 
+/*
+// for discord
+sealed case class GameState (parts: [Coord])
+  override def run(args: List[String]): ZIO[Console with Clock, Nothing, ExitCode] = {
+    val streamOfState: ZStream[Console with Clock, IOException, GameState]
+    def render(state: GameState): ZIO[Console, Nothing, Unit] 
+    for {
+      a <- streamOfState.tap(s => render(s)).either.fold(ZIO.succeed(()))((_,_) => ZIO.succeed(())).exitCode
+    } yield a
+  }
+}
+
+//when I update GameState, I need random numbers, or I need to pull from a stream of random numbers that's initialized when I initialize GameState. Whats the best way to do this?
+//
+//I could turn streamOfState to be of type: ZStream[Console with Clock, IOException, ZIO[Random, Nothing, GameState]]
+//but then how do I tap the stream and pass just GameState to render?
+//
+//I tried this:
+  val foodStream: ZStream[Random, Nothing, Food] = 
+    ZStream.repeatEffect {
+      for {
+        x <- nextIntBounded(width)
+        y <- nextIntBounded(height)
+      } yield Food(Position(x,y))
+    } 
+
+
+ZStream[Clock with Random, Nothing, Event] = ZStream.tick(500.millis).flatMap(_ => foodStream)
+
+// but it seemed to disregard the 500.millis tick and just produce the Food without any regard for a time step
+// I changed it to this out of curiousity:
+foodStream.flatMap(f => ZStream.tick(500.millis).map(_ => f))
+and this preserved the time step but the f became constant, instead of producing a new f at each tick 
+
+
+*/ 
 
